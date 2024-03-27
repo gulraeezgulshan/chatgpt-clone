1711239c7bdd54835499442002a228f634257bbf
Gulraeez Gulshan <gulraeezgulshan@gmail.com>
2024-03-27 10:17:28 +0500

    first commit

---
+++ constants/index.js
@@ -0,0 +1,51 @@
+export const menuLinks = [
+	{
+		label: "Clear conversations",
+		href: "/",
+		icon: "/assets/icons/delete.svg",
+	},
+	{
+		label: "Light mode",
+		href: "/",
+		icon: "/assets/icons/mode.svg",
+	},
+	{
+		label: "My account",
+		href: "/",
+		icon: "/assets/icons/profile.svg",
+	},
+	{
+		label: "Updates & FAQ",
+		href: "/",
+		icon: "/assets/icons/faq.svg",
+	},
+	{
+		label: "Log out",
+		href: "/",
+		icon: "/assets/icons/logout.svg",
+	},
+];
+
+
+export const chats = [
+    {
+		text: "AI Chat Tool Ethics",
+		href: "/",
+		icon: "/assets/icons/chat.svg",
+	},
+    {
+		text: "Al Chat Tool Impact Writing",
+		href: "/",
+		icon: "/assets/icons/chat.svg",
+	},
+    {
+		text: "New chat",
+		href: "/",
+		icon: "/assets/icons/chat.svg",
+	},
+    {
+		text: "How to become freelancer",
+		href: "/",
+		icon: "/assets/icons/chat.svg",
+	},
+]
